e92c0a77d4c0b82df35630d1dd208965d090aec5
BuddhiLW <pedrogbranquinho@gmail.com>
2022-11-21 17:12:45 -0300

    Add CV end-point at blog

--- src/cryogen/server.clj
+++ src/cryogen/server.clj
@@ -56,7 +56,9 @@
                        (when (= (:clean-urls config) :dirty)
                          "index.html")))))
   (GET "/cv" [] (ring.util.response/file-response
-                 (path "content/pdf/cv-2022-11-03.pdf")))
+                 (let [config (resolve-config)]
+                   (path (:blog-prefix config)
+                         "content/pdf/cv-2022-11-03.pdf"))))
   (route/files "/")
   (route/not-found "Page not found"))
 
